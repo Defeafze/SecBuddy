6afb58ac6e4071bbfa5e7bfa95b546a9524dcd1a
Defeafze <151008861+Defeafze@users.noreply.github.com>
2026-06-07 21:58:25 +0200

    version
    changed appversion in files

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@
 # SENTRY_DSN   findest du in deinem Sentry-Projekt unter:
 #              Settings → Projects → SecBuddy → Client Keys (DSN)
 
-APP_VERSION = "1.0.1"
+APP_VERSION = "1.0.2"
 
 SENTRY_DSN = "https://1ffe7b7d022c6d0043c36cdce21fd87d@o4511515261009920.ingest.de.sentry.io/4511515492024400"
 
